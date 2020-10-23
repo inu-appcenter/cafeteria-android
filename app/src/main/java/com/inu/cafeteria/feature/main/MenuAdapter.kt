96d6315126acf8c495574b67c1c52a712ec19fb2
potados99 <potados99@gmail.com>
2020-10-23 22:53:20 +0900

    Calorie and price is shown only when available.

--- app/src/main/java/com/inu/cafeteria/feature/main/MenuAdapter.kt
+++ app/src/main/java/com/inu/cafeteria/feature/main/MenuAdapter.kt
@@ -4,6 +4,7 @@ import android.view.ViewGroup
 import com.inu.cafeteria.R
 import com.inu.cafeteria.common.base.BaseViewHolder
 import com.inu.cafeteria.common.base.DefaultAdapter
+import com.inu.cafeteria.common.extension.setVisible
 import kotlinx.android.synthetic.main.menu.view.*
 import timber.log.Timber
 
@@ -19,17 +20,34 @@ class MenuAdapter : DefaultAdapter<MenuView>() {
         val menu = getItem(position) ?: return
 
         with(holder.view) {
-            available_at.setAvailableTime(menu.availableAt)
-            corner_name.text = menu.cornerName
-            foods.text = menu.foods
-            price.text = context.getString(R.string.unit_krw, menu.price)
-            calorie.text = context.getString(R.string.unit_cal, menu.calorie)
 
-            // Initial
-            foods.maxLines = 2
+            with(available_at) {
+                setAvailableTime(menu.availableAt)
+            }
 
-            // On click
-            setOnClickListener { foods.maxLines = 5 }
+            with(corner_name) {
+                text = menu.cornerName
+            }
+
+            with(foods) {
+                text = menu.foods
+
+                // Initial
+                maxLines = 2
+
+                // On click
+                setOnClickListener { maxLines = 5 }
+            }
+
+            with(price) {
+                setVisible(menu.price ?: 0 != 0)
+                text = context.getString(R.string.unit_krw, menu.price)
+            }
+
+            with(calorie) {
+                setVisible(menu.calorie ?: 0 != 0)
+                text = context.getString(R.string.unit_cal, menu.calorie)
+            }
         }
     }
 }
